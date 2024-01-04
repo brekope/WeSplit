9112583f32432d385794a9d32bee5fdcd750d032
Bre <breana.koppenhaver@gmail.com>
2024-01-04 15:32:39 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Breana Koppenhaver on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
